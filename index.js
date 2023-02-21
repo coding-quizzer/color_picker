52a276ddc395a88f9bffa24f2baaca42236566f0
William Gadd <wgadd@sasktel.net>
2023-02-21 12:53:01 -0600

    Create generic setColorInColorObj function, rename color object functions to color in color object functions and move the main function to the top of index.js

--- index.js
+++ index.js
@@ -1,72 +1,80 @@
 
 
+{
 
-const convertHexColorValueToString = (hexNum) => {
-  // Add 0x100 to preserve the zeros as padding in the final string values
-  let newColor = hexNum + 0x100;
-  newColor = newColor.toString(16).slice(1);
-  return newColor;
-};
+  $(() => {
 
-const createColorBox = (colorValues, $colorPickerBox, isFirstRow) => {
-  const [red, green, blue] = Object.values(colorValues).map(colorNum => convertHexColorValueToString(colorNum));
-  currentColor = `#${red}${green}${blue}`;
-  let $currentBlock = $('<div class = "color-block">')
-    .css("background-color", currentColor)
-    .appendTo($colorPickerBox);
+    const $colorGridBox = $("#color-grid-box");
 
-  if (isFirstRow) {
-    $colorPickerBox.css({ "--num-of-columns": "+=1" });
-  }
-
-  return $colorPickerBox;
+    generateColorGrid(colorBoxInitSettings, $colorGridBox);
 
 
-};
+  });
 
-const updateColorObj = (colorObj, updateColor, boxCount) => {
-  colorObj[updateColor] = Math.min(colorObj[updateColor] + Math.floor(256 / boxCount), 255);
 
-};
+  // const blockColors = [{ red: "00", green: "00", blue: "00" }];
+  // let currentColor = '9ACD32';
+  let colorObj = { red: 0x00, green: 0x00, blue: 0x00 };
 
-const resetColorObj = (colorObj, updateColor) => {
-  colorObj[updateColor] = 0;
+  const colorBoxInitSettings = {
+    size: { column: 8, row: 8 },
+    colors: { column: "red", row: "blue" }
+  };
 
-};
 
-const generateColorGrid = (initSettings, $colorPickerBox) => {
-  const { size, colors } = initSettings;
 
-  for (let i = 0; i < size.row; i++) {
-    $colorPickerBox.css("--num-of-rows", "+=1");
-    for (let j = 0; j < size.column; j++) {
-      createColorBox(currentColorObj, $colorPickerBox, i === 1);
-      updateColorObj(currentColorObj, colors.column, size.column);
+  const convertHexColorValueToString = (hexNum) => {
+    // Add 0x100 to preserve the zeros as padding in the final string values
+    let newColor = hexNum + 0x100;
+    newColor = newColor.toString(16).slice(1);
+    return newColor;
+  };
+
+  const createColorBox = (colorValues, $colorPickerBox, isFirstRow) => {
+    const [red, green, blue] = Object.values(colorValues).map(colorNum => convertHexColorValueToString(colorNum));
+    currentColor = `#${red}${green}${blue}`;
+    let $currentBlock = $('<div class = "color-block">')
+      .css("background-color", currentColor)
+      .appendTo($colorPickerBox);
+
+    if (isFirstRow) {
+      $colorPickerBox.css({ "--num-of-columns": "+=1" });
     }
-    resetColorObj(currentColorObj, colors.column);
-    console.log(currentColorObj);
 
-    updateColorObj(currentColorObj, colors.row, size.row);
-  }
-
-};
-
-const blockColors = [{ red: "00", green: "00", blue: "00" }];
-let currentColor = '9ACD32';
-let currentColorObj = { red: 0x00, green: 0x00, blue: 0x00 };
-
-const colorBoxInitSettings = {
-  size: { column: 8, row: 8 },
-  colors: { column: "red", row: "blue" }
-};
+    return $colorPickerBox;
 
 
-$(() => {
+  };
 
-  const $colorPickerBox = $("#color-picker-box");
+  const setColorInColorObj = (colorObj, colorName, newValue) => {
+    colorObj[colorName] = newValue;
+  };
 
-  generateColorGrid(colorBoxInitSettings);
+  const updateColorInColorObj = (colorObj, colorName, boxCount) => {
+    const nextColor = Math.min(colorObj[colorName] + Math.floor(256 / boxCount, 255));
+    setColorInColorObj(colorObj, colorName, nextColor);
+
+  };
+
+  const resetColorInColorObj = (colorObj, colorName) => {
+    setColorInColorObj(colorObj, colorName, 0);
+  };
+
+  const generateColorGrid = (initSettings, $colorPickerBox) => {
+    const { size, colors } = initSettings;
+
+    for (let i = 0; i < size.row; i++) {
+      $colorPickerBox.css("--num-of-rows", "+=1");
+      for (let j = 0; j < size.column; j++) {
+        createColorBox(colorObj, $colorPickerBox, i === 1);
+        updateColorInColorObj(colorObj, colors.column, size.column);
+      }
+      resetColorInColorObj(colorObj, colors.column);
+      updateColorInColorObj(colorObj, colors.row, size.row);
+    }
+
+  };
 
 
 
-});
+}
